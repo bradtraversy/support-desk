cd83d80c346261857a6311c81e96fe0f1183d9dd
Brad Traversy <traversymedia@gmail.com>
2022-01-13 11:15:42 -0500

    Frontend setup and initial pages

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  )
+}
+
+export default Register
