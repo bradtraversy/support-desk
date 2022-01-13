423342cde7577836d40203e5c5573872e95ea854
Brad Traversy <traversymedia@gmail.com>
2022-01-13 10:34:04 -0500

    Protect routes and authenticate with JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,6 +70,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  res.status(200).json(user)
+})
+
 // Generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -80,4 +92,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser } = require('../controllers/userController')
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController')
+
+const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
+router.get('/me', protect, getMe)
 
 module.exports = router
