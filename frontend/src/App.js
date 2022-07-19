73180c279d4aebd82c9caf661751960f3d7bdaed
Will Adams <bushblade@gmail.com>
2022-07-19 15:21:38 +0100

    No need for Outlet as not really nested route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,15 +20,30 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/new-ticket' element={<PrivateRoute />}>
-              <Route path='/new-ticket' element={<NewTicket />} />
-            </Route>
-            <Route path='/tickets' element={<PrivateRoute />}>
-              <Route path='/tickets' element={<Tickets />} />
-            </Route>
-            <Route path='/ticket/:ticketId' element={<PrivateRoute />}>
-              <Route path='/ticket/:ticketId' element={<Ticket />} />
-            </Route>
+            <Route
+              path='/new-ticket'
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path='/tickets'
+              element={
+                <PrivateRoute>
+                  <Tickets />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path='/ticket/:ticketId'
+              element={
+                <PrivateRoute>
+                  <Ticket />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </Router>
--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -4,12 +4,12 @@ import Spinner from './Spinner'
 
 // NOTE: no need for useAuthStatus as it's a duplicate of Redux state
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ children }) => {
   const { user, loading } = useSelector((state) => state.auth)
 
   if (loading) return <Spinner />
 
-  if (user) return <Outlet />
+  if (user) return <>{children}</>
 
   return <Navigate to='/login' />
 }
